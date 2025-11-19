e2963a94b1f0dd2732f7ba7c7e589a433e2f6099
jademchog <221308b@student.hci.edu.sg>
2025-11-19 15:50:19 +0800

    expansion

---
+++ vue-project/src/components/miau.vue
@@ -0,0 +1,7 @@
+<v-expansion-panels>
+    <v-expansion-panel
+      title="Title"
+      text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima"
+    >
+    </v-expansion-panel>
+  </v-expansion-panels>
--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,8 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import IEatPlanes from './components/IEatPlanes.vue'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-app = createApp(App)
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
+
+const app = createApp(App)
 app.component('i-eat-planes', IEatPlanes)
 app.mount('#app')
+app.use(vuetify)
 
