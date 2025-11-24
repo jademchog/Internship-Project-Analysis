22bae952293d7e0661b005c9a634be07f255547b
Ruxi <wk5crmrxbg@privaterelay.appleid.com>
2025-11-24 14:42:30 +0800

    Update DashBoard.vue

--- vuetify-project/src/pages/DashBoard.vue
+++ vuetify-project/src/pages/DashBoard.vue
@@ -1,5 +1,13 @@
 <template>
-Hello!
+    <v-app>
+        <v-app-bar :elevation="2" image="../assets/carhere.jpg">
+        <template v-slot:prepend>
+            <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+        </template>
+
+        <v-app-bar-title>Directory</v-app-bar-title>
+        </v-app-bar>
+    </v-app>
 </template>
 
 <script>
