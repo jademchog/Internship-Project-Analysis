df9b37a3a333bc8790f7dcc1a040af858adcc461
Ruxi <wk5crmrxbg@privaterelay.appleid.com>
2025-11-24 15:52:49 +0800

    cat avatar

--- vuetify-project/src/pages/DashBoard.vue
+++ vuetify-project/src/pages/DashBoard.vue
@@ -1,11 +1,27 @@
 <template>
-    <v-app>
-        <v-app-bar :elevation="2" image="../assets/carhere.jpg">
-        <template v-slot:prepend>
-            <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
-        </template>
-
-        <v-app-bar-title>Directory</v-app-bar-title>
-        </v-app-bar>
-    </v-app>
+  <v-app>
+    <v-row>
+        <v-col>
+            <v-card variant="tonal" class="card">i eat food</v-card>
+        </v-col>
+        <v-col>
+            <v-card variant="tonal" class="card">me too</v-card>
+        </v-col>
+    </v-row>
+  </v-app>
 </template>
+
+<script></script>
+
+<style>
+.card {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 15%;
+    margin: auto;
+    width: 40%;
+}
+
+
+</style>
