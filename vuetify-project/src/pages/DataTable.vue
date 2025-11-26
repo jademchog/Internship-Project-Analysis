b69728457e06e532231058d2da16f8429d4b0926
Ruxi <wk5crmrxbg@privaterelay.appleid.com>
2025-11-26 17:03:15 +0800

    s

--- vuetify-project/src/pages/DataTable.vue
+++ vuetify-project/src/pages/DataTable.vue
@@ -1,16 +1,53 @@
 <template>
+  <v-card height="10vh" width="120vh" border rounded color="red" class="title">
+    <h1>Database Query</h1>
+  </v-card>
+
   <v-data-table
     :headers="headers"
     :items="items"
-    height="80vh"
+    height="60vh"
     :items-per-page="10"
     fixed-header
   ></v-data-table>
+
+  <v-card 
+  class="my-4" 
+  title="Query the Database" 
+  subtitle="The table will display according to the University Name and Year"
+  min-height="30%">
+    <v-row>
+      <v-col>
+        <v-text-field
+        label="University Name"
+        :model-value="university"
+        @update:model-value="university = $event"
+        ></v-text-field>
+      </v-col>
+      <v-col>
+        <v-text-field
+        label="Year"
+        :model-value="year"
+        @update:model-value="year = $event"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-btn
+      class="search"
+      variant="tonal"
+      @click="querydb"
+      >Search</v-btn>
+    </v-row>
+  </v-card>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import axios from 'axios'
+
+const university=ref('')
+const year=ref('')
 
 const headers = [
   { title: 'School', value: 'school' },
@@ -19,11 +56,30 @@ const headers = [
 
 const items = ref([])
 
-axios.get("http://localhost:8081/api/v1/graduate-employment?university=Nanyang Technological University&year=2023")
-  .then(res => {
-    console.log(res.data) // check the structure
-    items.value = res.data.data || []
-  })
+function querydb () {
+  axios.get(`http://localhost:8081/api/v1/graduate-employment?university=${university.value}&year=${year.value}`)
+    .then(res => {
+      console.log(res.data) // check the structure
+      items.value = res.data.data || []
+    })
+}
+
 
 console.log(items)
 </script>
+
+<style>
+.search {
+  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.title{
+  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
